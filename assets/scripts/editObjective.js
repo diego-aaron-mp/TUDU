14665f0531b4d99ade8b065e2377e466c9373977
Diego Aarón Macías Padilla <daaron.macias@alumnos.udg.mx>
2022-05-04 11:52:29 -0500

    Script para ver ojvetivo

---
+++ assets/scripts/editObjective.js
@@ -0,0 +1,12 @@
+// Script para abrir modal de editar objetivo
+(function(){
+    $(document).ready(function () {
+        // Abre modal al dar click en boton
+        $('#btnEditObjective').click(function () {
+            $('#editModalObjective').modal('show');
+        });
+    });
+    
+
+}());
+
